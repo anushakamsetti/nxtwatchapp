0ccec7305ab425daaf72be9aad051013280c66db
anushakamsetti <annu.honey97@gmail.com>
2023-07-12 09:58:07 +0000

    first

---
+++ src/components/GameVideoCard/index.js
@@ -0,0 +1,40 @@
+import ThemeAndVideoContainer from '../../context/ThermeAndVideoContent'
+
+import {
+  ItemLink,
+  GamingListItem,
+  GamingThumbNailImage,
+  GamingContentSection,
+  GamingTitle,
+  GamingViewsAndDate,
+} from './styledComponent'
+
+const VideoCard = props => {
+  const {videoDetails} = props
+  const {id, title, thumbnailUrl, viewCount} = videoDetails
+
+  return (
+    <ThemeAndVideoContainer.Consumer>
+      {value => {
+        const {isDarkTheme} = value
+        const textColor = isDarkTheme ? '#f9f9f9' : '#231f20'
+
+        return (
+          <ItemLink to={`/videos/${id}`} className="link">
+            <GamingListItem>
+              <GamingThumbNailImage src={thumbnailUrl} alt="video thumbnail" />
+              <GamingContentSection>
+                <GamingTitle color={textColor}>{title}</GamingTitle>
+                <GamingViewsAndDate color={textColor}>
+                  {viewCount} Watching Worldwide
+                </GamingViewsAndDate>
+              </GamingContentSection>
+            </GamingListItem>
+          </ItemLink>
+        )
+      }}
+    </ThemeAndVideoContainer.Consumer>
+  )
+}
+
+export default VideoCard
